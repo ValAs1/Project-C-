b5b52126d5cbc5fabc2183e56f739401a4589328
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-03-19 16:28:07 +0400

    za

---
+++ Vis C-1/Vis C-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vis_C_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ValAs");
+                      
+            string inp = Console.ReadLine();    // input
+            int a = int.Parse(inp);
+            int b = a + 5;
+            Console.WriteLine(b);
+
+            int myInt1;
+            myInt1 = 0;
+
+            int myInt2;
+
+            Console.WriteLine($"myInt2: {myInt1}");
+            myInt2 = 369;
+            Console.WriteLine($"myInt2: {myInt2}");
+            int myInt3 = 15;
+            Console.WriteLine($"myInt3: {myInt3}");
+            double myDouble = 15.8;
+            Console.WriteLine($"myDouble: {myDouble}");
+            string myString = "Hello world";
+            Console.WriteLine(myString);
+            string myString1 = "Hello \n world";
+            Console.WriteLine(myString1);
+            char mychar = 'g';   //  1символ
+            Console.WriteLine(mychar);
+            var myT1 = "test";
+            Console.WriteLine(myT1);
+
+            Console.ReadLine();
+        }
+    }
+}
