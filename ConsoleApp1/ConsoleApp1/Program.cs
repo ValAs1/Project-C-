4ccccd63079ad8e58c552480489da0f96e2e7ee3
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-03-22 00:21:13 +0400

    22

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите трехзначное число ");
+            string r = Console.ReadLine();
+            int g = int.Parse(r);
+            int a = g % 10;
+            int b = g - a ;
+            int b1 = b % 100;
+            //int b = g - a * 100;
+            //int b1 = b / 10;
+            //int c = b - b1*10;
+            Console.WriteLine(a);
+            Console.WriteLine(b1);
+            //Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+    }
+}
