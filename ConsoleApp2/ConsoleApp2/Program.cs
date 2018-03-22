6a63a8f05bd3073adb4c2b0c3768f3bb3b00c722
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-03-22 19:38:04 +0400

    54

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("abc");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            // int b = a % 400;
+            //Console.WriteLine(b);
+
+            Console.Write("abc");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = a % 400;
+            Console.WriteLine(b);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
