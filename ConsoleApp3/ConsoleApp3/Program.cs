71c751c2283e28fcb67fc70c2fa97ae70d6ede65
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-03-23 17:33:23 +0400

    23.03

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите трехзначное число ");
+            string r = Console.ReadLine();
+            int g = int.Parse(r);
+            int a = g / 100;
+            int b = (g - (a * 100)) / 10;
+            int c = g - (a * 100) - (b * 10);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine($"{c}{b}{a}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/umnajenie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umnajenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("a * b =  ");
+            double ab = Convert.ToDouble(Console.ReadLine());
+            if ((a * b)== ab)
+                Console.WriteLine("true");
+           else
+                Console.WriteLine("false");
+           Console.ReadLine();
+
+        }
+    }
+}
