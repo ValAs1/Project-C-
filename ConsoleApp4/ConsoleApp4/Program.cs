e4c89026a224676219f99f466aaccf317060ae4e
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-04-03 08:06:14 +0400

    01

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private string hash(string text)
+            {
+                byte[] data = Encoding.Default.GetBytes(text);
+                var result = new SHA256Managed().ComputeHash(data);
+                return BitConverter.ToString(result).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
