196b2928335787de4e233066978233076d515bfb
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-04-03 19:34:10 +0400

    03,04

--- zangvac/ConsoleApp1/Program.cs
+++ zangvac/ConsoleApp1/Program.cs
@@ -10,18 +10,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("n=");
-            int n = int.Parse(Console.ReadLine());
+            ////Console.Write("n=");
+            ////int n = int.Parse(Console.ReadLine());
 
-            int[] Array = new int[n];
+            //int[] numbers = { 4, 7, 13, 20, 33, 23, 54 };
+            //int s = 0;
 
-            for (int i = 0; i <= n; i++)
-            {
-                Array[i] =i*i;
-                // Array[i] = i * i;
-                Console.Write(Array[i] + ", ");
-            }
-            Console.ReadLine();
+            //foreach (int el in numbers)
+            //{
+            //    s += el;
+            //}
+            //Console.WriteLine(s);
+
+            /////////////////
+            // Используя цикл foreach, выведите на экран все элементы массива целых чисел, которые больше 20 и меньше 50.
+
+            ////int[] numbers = { 4, 7, 13, 20, 33, 23, 54 };
+            ////foreach (int el in numbers)
+            ////{
+            ////   if (el<50 && el>20 )
+            ////        Console.WriteLine(el);
+            ////}
+
+
+
+
+
+
+                Console.ReadKey();
         }
     }
 }
---
+++ zangvac/tvanshanneri  qanak@/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tvanshanneri__qanak_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine( "n = " );
+            //int n =int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < length; i++)
+            //{
+
+            //}
+
+            ////////////////
+            //{
+            //    string text = "WindowsFormsApplication15";
+
+            //  //  int i = 0;
+            //    foreach (char ch in text)
+            //    {
+            //       // i = 0;
+            //        for (int i = 0; text.Length > i; i++)
+            //        {
+            //            if (ch == text[i])
+            //            {
+            //                i++;
+            //                Console.Write(ch);
+            //            }
+
+            //        }
+
+            //    }
+            //}
+            
+            Console.ReadLine();
+        }
+    }
+}
